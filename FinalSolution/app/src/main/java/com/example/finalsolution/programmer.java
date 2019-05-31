773f0a8f0a42c2f31dad320f0a666e89407d3b1d
scao7 <scao7@crimson.ua.edu>
2019-05-30 22:24:32 -0500

    k-mean

--- FinalSolution/app/src/main/java/com/example/finalsolution/programmer.java
+++ FinalSolution/app/src/main/java/com/example/finalsolution/programmer.java
@@ -22,7 +22,6 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
-import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.core.TermCriteria;
@@ -149,7 +148,7 @@ public class programmer extends AppCompatActivity {
                         cannyContoursThreshold();
                         break;
                     case 10:
-                        thresholdCannyContours();
+                        k_Mean();
                         break;
                         default:
                             setOriginal();
@@ -422,61 +421,65 @@ public class programmer extends AppCompatActivity {
     }
 
 
-//    public void k_Mean(){
-//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),images[current_image]);
-//        Mat img = new Mat();
-//        Utils.bitmapToMat(bitmap,img);
-//
-//        Imgproc.cvtColor(img,img,);
-//        int k = 2;
-//        Mat clusters = cluster(img, k).get(0);
-//
-//        Utils.matToBitmap(clusters,bitmap);
-//
-//
-//        imageView.setImageBitmap(bitmap);
-//    }
-//
-//    public static List<Mat> cluster(Mat cutout, int k) {
-//        Mat samples = cutout.reshape(1, cutout.cols() * cutout.rows());
-//        Mat samples32f = new Mat();
-//        samples.convertTo(samples32f, CvType.CV_32F, 1.0 / 255.0);
-//        Mat labels = new Mat();
-//        TermCriteria criteria = new TermCriteria(TermCriteria.COUNT, 100, 1);
-//        Mat centers = new Mat();
-//        Core.kmeans(samples32f, k, labels, criteria, 1, Core.KMEANS_PP_CENTERS, centers);
-//        return showClusters(cutout, labels, centers);
-//    }
-//    private static List<Mat> showClusters (Mat cutout, Mat labels, Mat centers) {
-//        centers.convertTo(centers, CvType.CV_8UC1, 255.0);
-//        centers.reshape(3);
-//        List<Mat> clusters = new ArrayList<Mat>();
-//        for(int i = 0; i < centers.rows(); i++) {
-//            clusters.add(Mat.zeros(cutout.size(), cutout.type()));
-//        }
-//        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
-//        for(int i = 0; i < centers.rows(); i++) counts.put(i, 0);
-//        int rows = 0;
-//        for(int y = 0; y < cutout.rows(); y++) {
-//            for(int x = 0; x < cutout.cols(); x++) {
-//                int label = (int)labels.get(rows, 0)[0];
-//                int r = (int)centers.get(label, 2)[0];
-//                int g = (int)centers.get(label, 1)[0];
-//                int b = (int)centers.get(label, 0)[0];
-//                clusters.get(label).put(y, x, b, g, r);
-//                rows++;
-//            }
-//        }
-//        return clusters;
-//    }
+    public void k_Mean(){
 
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),images[current_image]);
+        Mat img = Mat.zeros(bitmap.getHeight(), bitmap.getWidth(), CvType.CV_8UC3);
+
+//        Imgproc.rectangle(img, new Point(0, 0), new Point(100, 200), new Scalar(0, 255, 0), -1);
+//        Imgproc.rectangle(img, new Point(100, 0), new Point(200, 200), new Scalar(0, 0, 255), -1);
+
+        Mat clusters = cluster(img, 2).get(0);
+
+        Utils.matToBitmap(img,bitmap);
+        imageView.setImageBitmap(bitmap);
+
+    }
+
+    public static List<Mat> cluster(Mat cutout, int k) {
+        Mat samples = cutout.reshape(1, cutout.cols() * cutout.rows());
+        Mat samples32f = new Mat();
+        samples.convertTo(samples32f, CvType.CV_32F, 1.0 / 255.0);
+
+        Mat labels = new Mat();
+        TermCriteria criteria = new TermCriteria(TermCriteria.COUNT, 100, 1);
+        Mat centers = new Mat();
+        Core.kmeans(samples32f, k, labels, criteria, 1, Core.KMEANS_PP_CENTERS, centers);
+        return showClusters(cutout, labels, centers);
+    }
+
+    private static List<Mat> showClusters (Mat cutout, Mat labels, Mat centers) {
+        centers.convertTo(centers, CvType.CV_8UC1, 255.0);
+        centers.reshape(3);
+
+        List<Mat> clusters = new ArrayList<Mat>();
+        for(int i = 0; i < centers.rows(); i++) {
+            clusters.add(Mat.zeros(cutout.size(), cutout.type()));
+        }
+
+        Map<Integer, Integer> counts = new HashMap<Integer, Integer>();
+        for(int i = 0; i < centers.rows(); i++) counts.put(i, 0);
+
+        int rows = 0;
+        for(int y = 0; y < cutout.rows(); y++) {
+            for(int x = 0; x < cutout.cols(); x++) {
+                int label = (int)labels.get(rows, 0)[0];
+                int r = (int)centers.get(label, 2)[0];
+                int g = (int)centers.get(label, 1)[0];
+                int b = (int)centers.get(label, 0)[0];
+                counts.put(label, counts.get(label) + 1);
+                clusters.get(label).put(y, x, b, g, r);
+                rows++;
+            }
+        }
+        System.out.println(counts);
+        return clusters;
+    }
 
     private void thresholdCannyContours(){
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(),images[current_image]);
         Mat Rgba = new Mat();
         Mat ostuMat = new Mat();
-
-        Rect rectCrop = new Rect();
 
         Utils.bitmapToMat(bitmap,Rgba);
         Imgproc.cvtColor(Rgba,ostuMat,Imgproc.COLOR_RGBA2GRAY,0);
